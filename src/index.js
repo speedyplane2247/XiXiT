2dad9252d3b3eeec3cc9a053fd143a6b4891c340
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-28 13:40:45 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,29 @@ machineData.ramWORD = 8
 machineData.register = 16
 machineData.registerWORD = 32
 // Memory Init
-
+RAM.a1 = Array []
+RAM.r1 = Array []
+RAM.aCurr = 0
+RAM.rCurr = 0
+for (RAM.aCurr = 0; RAM.aCurr < machineData.ramSize; RAM.aCurr++) {
+  RAM.a1[RAM.aCurr] = ""
+}
+for (RAM.rCurr = 0; RAM.rCurr < machineData.register; RAM.rCurr++) {
+  RAM.r1[RAM.rCurr] = ""
+}
+// CPU Opcode
+function opCode(code) {
+alert("Failure on opCode #" + code + ".")
+}
 // Memory Add
+RAM.set = function(type, address, value) {
+  if (value.length > 8) {
+    
+  }
+}
+RAM.get = function(type, address) {
+  
+}
 // Machine CPU Functions
 CPU.add = function() {
   

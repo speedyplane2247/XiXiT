f731792d83bd3030c4c88f5b1c8144da23310c23
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-28 13:28:15 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@
 XiXiT | GNU License
 */
 // Important Processor Objects
-var processor = new Object()
+var CPU = new Object()
 var ram = new Object()
 var machineData = new Object()
+var RAM = new Object()
 // Machine Data Integers
 machineData.ramSize = 256
 machineData.ramWORD = 8
 machineData.register = 16
 machineData.registerWORD = 32
 // Memory Init
+
 // Memory Add
 // Machine CPU Functions
-processor.add = function() {
+CPU.add = function() {
   
 }
-processor.addn = function(word1, word2) {
+CPU.addn = function(word1, word2) {
   
 }
